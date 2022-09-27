689117c957ffe2ce4455b72e6e7de6948f7af139
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2022-09-27 10:07:16 -0300

    b

--- src/Library/ActiveWorkers.cs
+++ src/Library/ActiveWorkers.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 
 namespace ClassLibrary
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ActiveWorkers
     {
-        public List<Worker> activeWorkers = new List<Worker>();
+        public List<Worker>activeWorkers = new List<Worker>();
 
 
     }
